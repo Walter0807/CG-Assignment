aa30dd81b92a1af0fc85a2dff335f3753b329f05
Walter0807 <walterzhu@pku.edu.cn>
2018-10-01 15:47:42 +0800

    Hello Window Configuration

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,48 +1,78 @@
 // Local Headers
 #include "glitter.hpp"
 
+//// Console Color
+//#include "consoleColor.hpp"
+
 // System Headers
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 // Standard Headers
-#include <cstdio>
-#include <cstdlib>
+//#include <cstdio>
+//#include <cstdlib>
+#include <iostream>
+
+// 声明按键函数
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main(int argc, char * argv[]) {
-
-    // Load GLFW and Create a Window
+    
+    // glfw初始化
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 此行用来给Mac OS X系统做兼容
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
-
-    // Check for Valid Context
-    if (mWindow == nullptr) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
-        return EXIT_FAILURE;
+    
+    // 创建窗口,获取窗口上上下文
+    GLFWwindow* window = glfwCreateWindow(mWidth, mHeight, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
-
-    // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(mWindow);
+    glfwMakeContextCurrent(window);
+    
+    // 通过glfw注册按键事件回调
+    glfwSetKeyCallback(window, key_callback);
+    
+    // Load OpenGL Functions
     gladLoadGL();
-    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
-
-    // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
-
-        // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
+    //    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+    
+    // 获取视口
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    
+    // 渲染
+    while (!glfwWindowShouldClose(window))
+    {
+        // 检查事件
         glfwPollEvents();
-    }   glfwTerminate();
-    return EXIT_SUCCESS;
+        
+        // 渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // 交换缓冲
+        glfwSwapBuffers(window);
+    }
+    
+    // 释放GLFW分配的内存
+    glfwTerminate();
+    
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+    // 关闭应用程序
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
