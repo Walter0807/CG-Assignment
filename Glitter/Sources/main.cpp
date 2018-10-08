3a965be94b921895a66fa46331fbffd3b0aeaf01
Walter0807 <walterzhu@pku.edu.cn>
2018-10-08 20:20:47 +0800

    Submission

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -103,40 +103,33 @@ public:
     }
 };
 
-// 声明按键函数
+// Interact functions definition
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void do_movement();
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-
-// 相机参数
+// Camera Configuration
 glm::vec3 cameraPos = glm::vec3(-4.0f, 1.0f, 2.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-GLfloat yaw = -20.0f;    // Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right (due to how Eular angles work) so we initially rotate a bit to the left.
+GLfloat yaw = -20.0f;
 GLfloat pitch = 0.0f;
 GLfloat lastX = mWidth / 2.0;
 GLfloat lastY = mHeight / 2.0;
-//float fov   =  45.0f;
 bool keys[1024];
-
-// 时间增量Deltatime
 GLfloat deltaTime = 0.0f;    // Time between current frame and last frame
 GLfloat lastFrame = 0.0f;      // Time of last frame
 
-
 int main(int argc, char * argv[]) {
     
-    // glfw初始化
+    // glfw Initialization
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 此行用来给Mac OS X系统做兼容
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    
-    // 创建窗口,获取窗口上上下文
+    //Create a window
     GLFWwindow* window = glfwCreateWindow(mWidth, mHeight, "LearnOpenGL", nullptr, nullptr);
     if (window == nullptr)
     {
@@ -145,66 +138,126 @@ int main(int argc, char * argv[]) {
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
-    // 通过glfw注册按键事件回调
     glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     
     // Load OpenGL Functions
     gladLoadGL();
-    //    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    // OpenGL Version Info
     std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
-    
-    // 获取视口
+    // Global Configuration
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
-    
     glEnable(GL_DEPTH_TEST);
-    
-    // 编译着色器程序
-    Shader ourShader("vert.vert", "frag.frag");
-    
-    // 顶点输入
+    // Compile the shaders
+    Shader ourShader("VertexShaderCode.glsl", "FragmentShaderCode.glsl");
+    // Draw the vertices
     GLfloat vertices[] = {
-        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-        2.5f,  -0.51f, 2.5f,   0.4f, 0.4f, 0.4f,   // 右上
-        2.5f,  -0.51f, -2.5f,   0.4f, 0.4f, 0.4f,  // 右下
-        -2.5f, -0.51f, 2.5f,   0.4f, 0.4f, 0.4f,   // 左下
-        -2.5f, -0.51f, -2.5f,   0.4f, 0.4f, 0.4f, // 左上
+        //    Position + Color
+        2.5f,  -0.51f, 2.5f,   0.7f, 0.9f, 0.7f,
+        2.5f,  -0.51f, -2.5f,  0.7f, 0.9f, 0.7f,
+        -2.5f, -0.51f, 2.5f,   0.7f, 0.9f, 0.7f,
+        -2.5f, -0.51f, -2.5f,  0.7f, 0.9f, 0.7f,
         
-        1.5f, -0.5f, -0.5f,  1.0f, 0.4f, 0.4f,
-        1.5f, -0.5f, +0.5f,  0.4f, 1.0f, 0.4f,
-        1.5f,  0.5f, -0.5f,  0.4f, 0.4f, 1.0f,
-        1.5f,  0.5f, +0.5f,  0.4f, 1.0f, 0.4f,
+        1.5f, -0.5f, -0.5f,  1.00,0.91,0.25, //4
+        1.5f, -0.5f, +0.5f,  1.00,0.91,0.25,
+        1.5f,  0.5f, -0.5f,  1.00,0.91,0.25,
+        1.5f,  0.5f, +0.5f,  1.00,0.91,0.25,
         
-        -1.5f, -0.5f, -0.5f,  1.0f, 0.4f, 0.4f,
-        -1.5f, -0.5f, +0.5f,  0.4f, 1.0f, 0.4f,
-        -1.5f,  0.5f, -0.5f,  0.4f, 0.4f, 1.0f,
-        -1.5f,  0.5f, +0.5f,  0.4f, 1.0f, 0.4f,
+        -1.5f, -0.5f, -0.5f,  1.00,0.91,0.25,//8
+        -1.5f, -0.5f, +0.5f,  1.00,0.91,0.25,
+        -1.5f,  0.5f, -0.5f,  1.00,0.91,0.25,
+        -1.5f,  0.5f, +0.5f,  1.00,0.91,0.250,
         
-        0.7f, 0.9f, 0.0f,     0.0f, 0.0f, 0.0f,
-        -0.7f, 0.9f, 0.0f,     0.0f, 0.0f, 0.0f,
+        0.7f, 0.9f, 0.0f,     0.9f, 0.0f, 0.0f, //12
+        -0.7f, 0.9f, 0.0f,    0.9f, 0.0f, 0.0f,
+        
+        1.5f, -0.5f, 0.5f,  1.00,1.00,1.00, //14
+        -1.5f, -0.5f, 0.5f, 1.00,1.00,1.00,
+        1.5f, 0.5f, 0.5f,   1.00,1.00,1.00,
+        -1.5f, 0.5f, 0.5f,  1.00,1.00,1.00,
+        
+        1.5f, -0.5f, -0.5f,  1.00,1.00,1.00, //18
+        -1.5f, -0.5f, -0.5f, 1.00,1.00,1.00,
+        1.5f, 0.5f, -0.5f,   1.00,1.00,1.00,
+        -1.5f, 0.5f, -0.5f,  1.00,1.00,1.00,
+        
+        1.5f,  0.5f, -0.5f,  0.90,0.49,0.48, //22
+        1.5f,  0.5f, +0.5f,  0.90,0.49,0.48,
+        -1.5f,  0.5f, -0.5f, 0.90,0.49,0.48,
+        -1.5f,  0.5f, +0.5f, 0.90,0.49,0.48,
+        
+        0.5f, -0.5f, 0.5f,   0.55f, 0.55f, 0.55f,//26
+        -0.5f, -0.5f, 0.5f,  0.55f, 0.55f, 0.55f,
+        0.5f, -0.5f, 2.5f,   0.55f, 0.55f, 0.55f,
+        -0.5f, -0.5f, 2.5f,  0.55f, 0.55f, 0.55f,
+        
+        -0.3f, -0.5f, 0.5f, 0.83,0.53,0.68, //30
+        -0.3f, 0.3f, 0.5f,  0.83,0.53,0.68,
+         0.3f, 0.3f, 0.5f,  0.83,0.53,0.68,
+         0.3f, -0.5f, 0.5f, 0.83,0.53,0.68,
+        
+        -1.0f, -0.5f, 1.36f,  0.87,0.43,0.53, //34
+        -1.2f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        -1.75f, -0.5f, 1.27f,  0.87,0.43,0.53,
+        -1.54f, -0.5f, 1.46f,  0.87,0.43,0.53,
+        -1.47f, -0.5f, 1.86f,  0.87,0.43,0.53,
+        -1.12f, -0.5f, 1.68f,  0.87,0.43,0.53,
+        -1.3f, -0.5f, 1.16f,  0.87,0.43,0.53,
+        -1.6f, -0.5f, 1.96f,  0.87,0.43,0.53,
+        -1.1f, -0.5f, 1.6f,  0.87,0.43,0.53,
+        -1.11f, -0.5f, 1.2f,  0.87,0.43,0.53,
+        -2.0f, -0.5f, 1.56f,  0.87,0.43,0.53, //44
+        -1.2f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        -1.45f, -0.5f, 1.87f,  0.87,0.43,0.53,
+        -1.54f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        -1.27f, -0.5f, 1.36f,  0.87,0.43,0.53,
+        -2.02f, -0.5f, 1.38f,  0.87,0.43,0.53,
+        -1.1f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        -1.6f, -0.5f, 1.06f,  0.87,0.43,0.53,
+        -1.49f, -0.5f, 1.63f,  0.87,0.43,0.53,
+        -1.13f, -0.5f, 1.24f,  0.87,0.43,0.53,
+        
+        1.0f, -0.5f, 1.26f,  0.87,0.43,0.53, //54
+        1.2f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        1.75f, -0.5f, 1.22f,  0.87,0.43,0.53,
+        1.54f, -0.5f, 1.96f,  0.87,0.43,0.53,
+        1.47f, -0.5f, 1.46f,  0.87,0.43,0.53,
+        1.12f, -0.5f, 1.65f,  0.87,0.43,0.53,
+        1.3f, -0.5f, 1.75f,  0.87,0.43,0.53,
+        1.6f, -0.5f, 1.96f,  0.87,0.43,0.53,
+        1.1f, -0.5f, 1.6f,  0.87,0.43,0.53,
+        1.11f, -0.5f, 1.22f,  0.87,0.43,0.53,
+        2.0f, -0.5f, 1.56f,  0.87,0.43,0.53, //64
+        1.2f, -0.5f, 1.22f,  0.87,0.43,0.53,
+        1.45f, -0.5f, 1.82f,  0.87,0.43,0.53,
+        1.54f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        1.27f, -0.5f, 1.92f,  0.87,0.43,0.53,
+        2.02f, -0.5f, 1.38f,  0.87,0.43,0.53,
+        1.1f, -0.5f, 1.76f,  0.87,0.43,0.53,
+        1.6f, -0.5f, 1.26f,  0.87,0.43,0.53,
+        1.49f, -0.5f, 1.63f,  0.87,0.43,0.53,
+        1.13f, -0.5f, 1.04f,  0.87,0.43,0.53,
+        
     };
-    
-    GLuint indices[] = { // 注意索引从0开始!
+    //Draw the objects with indexing.
+    GLuint indices[] = {
         0, 1, 2, 3,
         4, 5, 6, 7,
         8, 9, 10, 11,
-        4, 5, 8, 9,
-        6, 7, 10, 11,
-        4, 6, 8, 10,
-        5, 7, 9, 11,
-        11,10,13,6,12,7,13,11,
-        
+        25,24,13,22,
+        12,23,13,25,
+        14,15,16,17,
+        18,19,20,21,
+        26,27,28,29,
+        30,31,32,33,
+        34,35,36,37,38,39,40,41,42,43,
+        44,45,46,47,48,49,50,51,52,53,
+        54,55,56,57,58,59,60,61,62,63,
+        64,65,66,67,68,69,70,71,72,73,
     };
-    
 
-    
-    
-    // 顶点数组对象 Vertex Array Object, VAO
-    // 顶点缓冲对象 Vertex Buffer Object，VBO
-    // 索引缓冲对象：Element Buffer Object，EBO或Index Buffer Object，IBO
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -218,45 +271,49 @@ int main(int argc, char * argv[]) {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    // 设置顶点position属性指针
+    // Set vertices position pointer
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
     
-    // 设置顶点color属性指针
+    // Set vertices color pointer
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
     
-    
+    glEnable( GL_LINE_SMOOTH );
     glBindVertexArray(0);
     
-    // 渲染
+    // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
         GLfloat currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-//        std::cout << deltaTime << std::endl;
-        // 检查事件
+       // Process Inputs
         glfwPollEvents();
         do_movement();
-        // 渲染指令
+
         glClearColor(0.9f, 0.9f, 0.9f, 0.6f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        // 绘图
         ourShader.Use();
         glBindVertexArray(VAO);
+        // Draw a Plane (grass)
         glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, 0);
-        glDrawElements(GL_TRIANGLE_STRIP, 24, GL_UNSIGNED_INT, (GLvoid*)(4 * sizeof(GL_UNSIGNED_INT)));
-        glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_INT, (GLvoid*)(28 * sizeof(GL_UNSIGNED_INT)));
-        glBindVertexArray(0);
+        // Draw a House
+        glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_INT, (GLvoid*)(4 * sizeof(GL_UNSIGNED_INT)));
+        glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_INT, (GLvoid*)(12 * sizeof(GL_UNSIGNED_INT)));
+        glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_INT, (GLvoid*)(20 * sizeof(GL_UNSIGNED_INT)));
+        glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, (GLvoid*)(28 * sizeof(GL_UNSIGNED_INT)));
+        // Draw a road
+        glDrawElements(GL_LINE_STRIP, 4, GL_UNSIGNED_INT, (GLvoid*)(32 * sizeof(GL_UNSIGNED_INT)));
+        // Draw flowers
+        glPointSize(4.0f);
+        glDrawElements(GL_POINTS, 40, GL_UNSIGNED_INT, (GLvoid*)(36 * sizeof(GL_UNSIGNED_INT)));
+
         
-        // 变换
+        // Transformation
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-//        model = glm::rotate(model, -45.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-//        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
         view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         projection = glm::perspective(45.0f, (GLfloat)mWidth / (GLfloat)mHeight, 0.1f, 100.0f);
         // Get their uniform location
@@ -266,28 +323,23 @@ int main(int argc, char * argv[]) {
         // Pass them to the shaders
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // Note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        
-        // 交换缓冲
         glfwSwapBuffers(window);
     }
     
-    // 释放VAO,VBO
+    // Free
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    
-    // 释放GLFW分配的内存
     glfwTerminate();
     
     return 0;
 }
 
+// Get keyboard inputs
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
-    // 关闭应用程序
+    // Esc ==> Exit
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
     if (key >= 0 && key < 1024)
@@ -299,29 +351,25 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
+// Move the camera.
 void do_movement()
 {
-    // 摄像机控制
     GLfloat cameraSpeed = 5.0f * deltaTime;
     if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
-//        cameraPos += cameraSpeed * cameraFront;
         cameraPos += cameraUp * cameraSpeed;
     if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
-//        cameraPos -= cameraSpeed * cameraFront;
         cameraPos -= cameraUp * cameraSpeed;
     if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-//    std::cout << keys[GLFW_KEY_UP] << std::endl;
-    
     if (keys[GLFW_KEY_EQUAL])
                 cameraPos += cameraSpeed * cameraFront;
     if (keys[GLFW_KEY_MINUS])
                 cameraPos -= cameraSpeed * cameraFront;
 }
 
-
+// Mouse ==> angle adjustment
 bool firstMouse = true;
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
@@ -333,18 +381,18 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
     
     GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos; // 注意这里是相反的，因为y坐标是从底部往顶部依次增大的
+    GLfloat yoffset = lastY - ypos;
     lastX = xpos;
     lastY = ypos;
     
-    GLfloat sensitivity = 0.05;    // 灵敏度
+    GLfloat sensitivity = 0.1;
     xoffset *= sensitivity;
     yoffset *= sensitivity;
     
     yaw += xoffset;
     pitch += yoffset;
     
-    // 对于俯仰角，要让用户不能看向高于89度的地方（在90度时视角会发生逆转，所以我们把89度作为极限） flipped
+// Clamped
     if (pitch > 89.0f)
         pitch = 89.0f;
     if (pitch < -89.0f)
